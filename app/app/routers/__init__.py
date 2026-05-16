e5235eb27fda3b0765f1c877cf348bd3626e90c2
K Sathvik <sathvikkadiyala7@gmail.com>
2026-05-16 13:56:42 +0530

    Add __init__.py component

---
+++ app/app/routers/__init__.py
@@ -0,0 +1,5 @@
+from .indexRouter import router as indexRouter
+from .datasetRouter import router as datasetRouter
+from .apikeyRouter import router as apikeyRouter
+from .searchRouter import router as searchRouter
+from .userRouter import router as userRouter
